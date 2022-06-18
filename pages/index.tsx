d7c7773354f1da4f00f4d4cde74225da2d2e5af9
Filip Troníček <filip@gitpod.io>
2022-06-18 18:21:34 +0000

    Write

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAccount } from "wagmi";
+import { useAccount, useContract, useContractWrite, useProvider } from "wagmi";
 import { create } from "ipfs-http-client";
 import { getClipHash } from "../lib/generateID";
+import { getContract } from "@wagmi/core";
 
 const client = create({
   host: "ipfs.infura.io",
@@ -15,12 +16,46 @@ const uploadToIPFS = async (data: any) => {
   return await client.add(JSON.stringify(data));
 };
 
+const contractAbi = [
+  { inputs: [], name: "AlreadyRegistered", type: "error" },
+  { inputs: [], name: "WrongLength", type: "error" },
+  {
+    inputs: [{ internalType: "string", name: "code", type: "string" }],
+    name: "retrieve",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "string", name: "code", type: "string" },
+      { internalType: "string", name: "ipfs", type: "string" },
+    ],
+    name: "store",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
 const Home: NextPage = () => {
   const { data } = useAccount();
   const [clipURL, setURL] = useState<string>("");
-  const clipHash = clipURL && getClipHash(clipURL).slice(0, 5);
+  const [code, setCode] = useState<string | null>(null);
   const [cid, setCID] = useState<string>();
   const [status, setStatus] = useState<string | boolean>(false);
+  const provider = useProvider();
+  const { data: contractData, isError: contractIsError, isLoading: contractIsLoading, writeAsync: writeContract }  = useContractWrite(
+    {
+      addressOrName: "0xb3bEC15056BD6ED275B261342BeDc666C38e1007",
+      contractInterface: contractAbi,
+      signerOrProvider: provider,
+    },
+    "store",
+    {
+      args: [code, cid]
+    }
+  );
   return (
     <div className="py-6 justify-center text-center">
       <div className="flex justify-center">
@@ -41,15 +76,23 @@ const Home: NextPage = () => {
         method="GET"
         onSubmit={async () => {
           setStatus("Uploading to IPFS");
-          setCID(
-            (
-              await uploadToIPFS({
-                url: clipURL,
-                code: clipHash,
-                owner: data?.address || undefined,
-              })
-            ).path
-          );
+          const creation = new Date().toISOString();
+          const clipHash = getClipHash(
+            `${clipURL}-${creation}-${data?.address || "anon"}`
+          ).slice(0, 5);
+          const cid = (
+            await uploadToIPFS({
+              url: clipURL,
+              code: clipHash,
+              createdAt: creation,
+              owner: data?.address || undefined,
+            })
+          ).path;
+          setCID(cid);
+          setCode(clipHash);
+
+          setStatus("Executing contract");
+          await writeContract({args: [clipHash, cid]});
           setStatus(false);
 
           return false;
@@ -85,7 +128,7 @@ const Home: NextPage = () => {
                 }
                 className={`p-3 ${!cid ? "text-red-600" : ""}`}
               >
-                {clipHash}
+                {code}
               </span>
             </span>
           </>
